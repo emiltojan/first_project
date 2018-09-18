5436010e8cd31fb2ea4ba64f531c8944446ccfe3
emiltojan <emiltojan194@gmail.com>
2018-09-18 05:45:17 +0530

    Add files via upload

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ColDef, ColumnApi, GridApi} from 'ag-grid-community';
 import {UserService} from '../../services/user.service';
 import {User} from '../../user';
 
@@ -9,15 +10,43 @@ import {User} from '../../user';
   providers:[UserService]
 })
 export class UserComponent implements OnInit {
-  users:User[];
+  private rowData:User[];
+  private columnDefs: ColDef[];
+
+// gridApi and columnApi
+  private api: GridApi;
+  private columnApi: ColumnApi;
 
   constructor(private userService:UserService){
-    this.userService.getUser().subscribe(users=>{
-        this.users = users;
-    })
+    this.columnDefs = this.createColumnDefs();
 }
 
   ngOnInit() {
+    this.userService.getUser().subscribe(
+      users => {
+          this.rowData = users;
+      },
+      error => {
+          console.log(error);
+      }
+  )
   }
 
+
+    // one grid initialisation, grap the APIs and auto resize the columns to fit the available space
+    onGridReady(params): void {
+        this.api = params.api;
+        this.columnApi = params.columnApi;
+
+        this.api.sizeColumnsToFit();
+    }
+
+    // create some simple column definitions
+    private createColumnDefs() {
+        return [
+            {field: '_id'},
+            {field: 'firstName'},
+            {field: 'lastName'}   
+        ]
+    }
 }
